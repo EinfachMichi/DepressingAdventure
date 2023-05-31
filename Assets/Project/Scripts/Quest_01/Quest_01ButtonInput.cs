396c31a811fe8b6ad1a18ad16e65ed5a1ebb057a
Nico-Stern <nstern2002@gmail.com>
2023-05-31 13:32:47 +0200

    Quest_01

--- Assets/Project/Scripts/Quest_01/Quest_01ButtonInput.cs
+++ Assets/Project/Scripts/Quest_01/Quest_01ButtonInput.cs
@@ -7,19 +7,22 @@ using UnityEngine.UI;
 public class Quest_01ButtonInput : MonoBehaviour
 {
     private int round;
+    private int choose; 
     public GameObject[] Button; 
 
     public void PlayerChoose(int choose)
     {
         //none=0, schere=1, Stein=2, Papier=3
         round++;
-        
+        this.choose = choose;
+        StartRound();
     }
 
     private void StartRound()
     {
-        Button[0].GetComponent<Button>().interactable = false;
-        Button[1].GetComponent<Button>().interactable = false;
-        Button[2].GetComponent<Button>().interactable = false;
+        for (int i = 0; i < Button.Length; i++)
+        {
+            Button[i].GetComponent<Button>().interactable = false;
+        } 
     }
 }
