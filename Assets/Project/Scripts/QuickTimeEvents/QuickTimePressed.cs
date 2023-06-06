7733c110614205909346b6c7fb788883b40be196
Nico-Stern <nstern2002@gmail.com>
2023-06-06 13:08:13 +0200

    Quicktimeevent

---
+++ Assets/Project/Scripts/QuickTimeEvents/QuickTimePressed.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuickTimePressed : MonoBehaviour
+{
+
+    //2Parameter schauen oder fragen
+
+    public void PressA(InputAction.CallbackContext value)
+    {
+        
+        print("a");
+    }
+
+    public void PressS(InputAction.CallbackContext value)
+    {
+
+    }
+
+    public void PressD(InputAction.CallbackContext value)
+    {
+
+    }
+
+    public void PressQ(InputAction.CallbackContext value)
+    {
+
+    }
+
+    public void PressW(InputAction.CallbackContext value)
+    {
+
+    }
+
+    public void PressE(InputAction.CallbackContext value)
+    {
+
+    }
+}
