c824c7155373daf053274ab631b5c47748409c51
EinfachMichi <94147049+EinfachMichi@users.noreply.github.com>
2023-06-12 16:35:10 +0200

    Only 200 Files. Hahaha NO PROBLEMO

---
+++ Assets/Project/Scripts/AI/Iris.cs
@@ -0,0 +1,15 @@
+﻿namespace AI
+{
+    public class Iris : NPC
+    {
+        private void Start()
+        {
+            interactable = true;
+        }
+
+        public override void Interaction()
+        {
+            
+        }
+    }
+}
